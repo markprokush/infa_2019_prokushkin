8b04d72ca591ccd0c54086e2d05a58a394bdf72c
markprokush <mark.prokushkin@mail.ru>
2019-09-20 15:19:20 +0300

    add file

---
+++ lab3/picture3.py
@@ -0,0 +1,20 @@
+from graph import *
+
+penSize(3)
+penColor('black')
+brushColor('yellow')
+circle(200,200,100)
+brushColor('red')
+penColor('black')
+circle(160,170,20)
+circle(230,170,17)
+brushColor('black')
+circle(160,170,10)
+circle(230,170,7)
+
+penSize(13)
+line(205,160,270,120)
+line(180,160,110,120)
+line(160,240,230,240)
+
+run()
---
+++ lab3/picture4.py
@@ -0,0 +1,9 @@
+from graph import *
+windowSize(1000, 1000)
+penSize(3)
+c = canvas()
+c.create_oval(200, 500, 400, 600, outline='grey', fill='brown', width=2)
+c.create_oval(350,550,400,600)
+run()
+
+
