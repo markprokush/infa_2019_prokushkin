aebc0ad27f198cf0d5542725df0207c4988ce00b
markprokush <mark.prokushkin@mail.ru>
2019-09-26 23:55:56 +0300

    add file

--- lab3/picture4.py
+++ lab3/picture4.py
@@ -15,6 +15,26 @@ rectangle(60, 0, 170, 600)
 rectangle(400, 0, 440, 430)
 rectangle(470, 0, 660, 470)
 
+import math
+
+
+def needle(x1, y1, theta):  # иголка
+    penSize(2)
+    penColor('black')
+    brushColor('#290000')
+    polygon([(0 * math.cos(theta) + 0 * math.sin(theta) + x1, -0 * math.sin(theta) + 0 * math.cos(theta) + y1),
+             (16 * math.cos(theta) + 0 * math.sin(theta) + x1, -16 * math.sin(theta) + 0 * math.cos(theta) + y1),
+             (8 * math.cos(theta) + (-70) * math.sin(theta) + x1, -8 * math.sin(theta) + (-70) * math.cos(theta) + y1)])
+
+
+needle(313, 520, 3.14 / 4)
+needle(323, 522, 3.14 / 6)
+needle(333, 515, 0 / 18)
+needle(348, 506, 0 / 18)
+needle(365, 509, -3.14 / 10)
+needle(383, 513, -3.14 / 6)
+needle(401, 512, -3.14 / 4)
+
 penSize(3)
 c.create_oval(432, 510, 455, 525, outline='white', fill='#50292a', width=1)  # правая верхняя нога
 c.create_oval(300, 510, 322, 525, outline='white', fill='#50292a', width=1)  # левая верхняя нога
@@ -28,4 +48,42 @@ c.create_oval(440, 503, 448, 511, outline='white', fill='black', width=1)  # г�
 
 c.create_oval(417, 525, 440, 540, outline='white', fill='#50292a', width=1)  # правая нижняя нога
 c.create_oval(305, 525, 328, 540, outline='white', fill='#50292a', width=1)  # левая нижняя нога
+
+needle(320, 500, 3.14 / 6)
+needle(335, 493, 3.14 / 18)
+needle(345, 490, 0 / 18)
+needle(360, 485, 0 / 18)
+needle(377, 485, -3.14 / 36)
+needle(395, 490, -3.14 / 18)
+needle(413, 490, -3.14 / 9)
+
+penSize(0)
+c.create_oval(365,455,380,425, outline='white', fill='white', width=0)
+c.create_oval(345,415,400,430, outline='white', fill='red', width=0)
+c.create_oval(355,417,365,420, outline='white', fill='white', width=1)
+c.create_oval(370,421,376,424, outline='white', fill='white', width=1)
+c.create_oval(379,417,389,420, outline='white', fill='white', width=1)
+c.create_oval(300, 455, 330, 485, outline='white', fill='#fe8800', width=0)
+
+needle(320, 512, 3.14 / 3)
+needle(320, 508, 3.14 / 50)
+needle(330, 505, 0 / 18)
+needle(355, 500, 0 / 18)
+needle(367, 500, -3.14 / 25)
+needle(380, 504, -3.14 / 12)
+needle(412, 505, -3.14 / 7)
+
+c.create_oval(380, 450, 420, 490, outline='white', fill='red', width=0)
+c.create_oval(320, 450, 350, 480, outline='white', fill='#fe8800', width=0)
+
+needle(320, 525, 3.14 / 6)
+needle(330, 527, 3.14 / 12)
+needle(340, 520, 0 / 18)
+needle(355, 513, 0 / 18)
+needle(372, 515, -3.14 / 25)
+needle(390, 518, -3.14 / 12)
+needle(408, 520, -3.14 / 4)
+
+
+
 run()
