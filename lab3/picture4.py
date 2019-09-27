1ce5db9955a9ef2c1a77f3b04505069d6248dc96
markprokush <mark.prokushkin@mail.ru>
2019-09-27 15:12:26 +0300

    add file

--- lab3/picture4.py
+++ lab3/picture4.py
@@ -17,82 +17,83 @@ rectangle(470, 0, 660, 470)
 
 import math
 
+def hedgehog(a, x, y):
+    def needle(x1, y1, theta):  # иголка
+        penSize(2)
+        penColor('black')
+        brushColor('#290000')
+        polygon([(a*0 * math.cos(theta) + a*0 * math.sin(theta) + x1, a*(-0) * math.sin(theta) + a*0 * math.cos(theta) + y1),
+                (a*16 * math.cos(theta) + a*0 * math.sin(theta) + x1, a*(-16) * math.sin(theta) + a*0 * math.cos(theta) + y1),
+                (a*8 * math.cos(theta) + a*(-70) * math.sin(theta) + x1, a*(-8) * math.sin(theta) + a*(-70) * math.cos(theta) + y1)])
 
-def needle(x1, y1, theta):  # иголка
-    penSize(2)
-    penColor('black')
-    brushColor('#290000')
-    polygon([(0 * math.cos(theta) + 0 * math.sin(theta) + x1, -0 * math.sin(theta) + 0 * math.cos(theta) + y1),
-             (16 * math.cos(theta) + 0 * math.sin(theta) + x1, -16 * math.sin(theta) + 0 * math.cos(theta) + y1),
-             (8 * math.cos(theta) + (-70) * math.sin(theta) + x1, -8 * math.sin(theta) + (-70) * math.cos(theta) + y1)])
 
+    needle(a*(-62)+x, a*15+y, 3.14 / 4)
+    needle(a*(-52)+x, a*17+y, 3.14 / 6)
+    needle(a*(-42)+x, a*10+y, 0 / 18)
+    needle(a*(-27)+x, a*1+y, 0 / 18)
+    needle(a*(-10)+x, a*4+y, -3.14 / 10)
+    needle(a*8+x, a*8+y, -3.14 / 6)
+    needle(a*26+x, a*7+y, -3.14 / 4)
 
-needle(313, 520, 3.14 / 4)
-needle(323, 522, 3.14 / 6)
-needle(333, 515, 0 / 18)
-needle(348, 506, 0 / 18)
-needle(365, 509, -3.14 / 10)
-needle(383, 513, -3.14 / 6)
-needle(401, 512, -3.14 / 4)
+    penSize(3)
+    c.create_oval(a*57+x, a*5+y, a*80+x, a*20+y, outline='white', fill='#50292a', width=1)  # правая верхняя нога
+    c.create_oval(a*(-75)+x, a*5+y, a*(-53)+x, a*20+y, outline='white', fill='#50292a', width=1)  # левая верхняя нога
 
-penSize(3)
-c.create_oval(432, 510, 455, 525, outline='white', fill='#50292a', width=1)  # правая верхняя нога
-c.create_oval(300, 510, 322, 525, outline='white', fill='#50292a', width=1)  # левая верхняя нога
+    c.create_oval(a*(-75)+x, a*(-35)+y, a*75+x, a*35+y, outline='white', fill='#50292a', width=1)  # туловище
+    c.create_oval(a*45+x, a*(-10)+y, a*95+x, a*15+y, outline='white', fill='#50292a', width=1)  # голова
 
-c.create_oval(300, 470, 450, 540, outline='white', fill='#50292a', width=1)  # туловище
-c.create_oval(420, 495, 470, 520, outline='white', fill='#50292a', width=1)  # голова
+    c.create_oval(a*90+x, a*(-2)+y, a*95+x, a*3+y, outline='white', fill='#50292a', width=1)  # нос
+    c.create_oval(a*73+x, a*(-8)+y, a*81+x, a*0+y, outline='white', fill='black', width=1)  # глаз 1
+    c.create_oval(a*65+x, a*(-2)+y, a*73+x, a*6+y, outline='white', fill='black', width=1)  # глаз 2
 
-c.create_oval(465, 503, 470, 508, outline='white', fill='#50292a', width=1)  # нос
-c.create_oval(448, 497, 456, 505, outline='white', fill='black', width=1)  # глаз 1
-c.create_oval(440, 503, 448, 511, outline='white', fill='black', width=1)  # глаз 2
+    c.create_oval(a*42+x, a*20+y, a*65+x, a*35+y, outline='white', fill='#50292a', width=1)  # правая нижняя нога
+    c.create_oval(a*(-70)+x, a*20+y, a*(-47)+x, a*35+y, outline='white', fill='#50292a', width=1)  # левая нижняя нога
 
-c.create_oval(417, 525, 440, 540, outline='white', fill='#50292a', width=1)  # правая нижняя нога
-c.create_oval(305, 525, 328, 540, outline='white', fill='#50292a', width=1)  # левая нижняя нога
+    #   иголки за туловищем
 
-#   иголки за туловищем
+    needle(a*(-55)+x, a*(-5)+y, 3.14 / 6)
+    needle(a*(-40)+x, a*(-12)+y, 3.14 / 18)
+    needle(a*(-30)+x, a*(-15)+y, 0 / 18)
+    needle(a*(-15)+x, a*(-20)+y, 0 / 18)
+    needle(a*2+x, a*(-20)+y, -3.14 / 36)
+    needle(a*20+x, a*(-15)+y, -3.14 / 18)
+    needle(a*38+x, a*(-15)+y, -3.14 / 9)
 
-needle(320, 500, 3.14 / 6)
-needle(335, 493, 3.14 / 18)
-needle(345, 490, 0 / 18)
-needle(360, 485, 0 / 18)
-needle(377, 485, -3.14 / 36)
-needle(395, 490, -3.14 / 18)
-needle(413, 490, -3.14 / 9)
+    #   яблоко и грибок
 
-#   яблоко и грибок
+    c.create_oval(a*(-10)+x,a*(-50)+y,a*5+x,a*(-80)+y, outline='white', fill='white', width=0)
+    c.create_oval(a*(-30)+x,a*(-90)+y,a*25+x,a*(-75)+y, outline='white', fill='red', width=0)
+    c.create_oval(a*(-20)+x,a*(-88)+y,a*(-10)+x,a*(-85)+y, outline='white', fill='white', width=1)
+    c.create_oval(a*(-5)+x,a*(-84)+y,a*1+x,a*(-81)+y, outline='white', fill='white', width=1)
+    c.create_oval(a*4+x,a*(-88)+y,a*14+x,a*(-85)+y, outline='white', fill='white', width=1)
+    c.create_oval(a*(-75)+x, a*(-50)+y, a*(-45)+x, a*(-20)+y, outline='white', fill='#fe8800', width=0)
 
-c.create_oval(365,455,380,425, outline='white', fill='white', width=0)
-c.create_oval(345,415,400,430, outline='white', fill='red', width=0)
-c.create_oval(355,417,365,420, outline='white', fill='white', width=1)
-c.create_oval(370,421,376,424, outline='white', fill='white', width=1)
-c.create_oval(379,417,389,420, outline='white', fill='white', width=1)
-c.create_oval(300, 455, 330, 485, outline='white', fill='#fe8800', width=0)
+    #   иголки перед туловищем
 
-#   иголки перед туловищем
+    needle(a*(-55)+x, a*7+y, 3.14 / 3)
+    needle(a*(-55)+x, a*3+y, 3.14 / 50)
+    needle(a*(-45)+x, a*0+y, 0 / 18)
+    needle(a*(-20)+x, a*(-5)+y, 0 / 18)
+    needle(a*(-8)+x, a*(-5)+y, -3.14 / 25)
+    needle(a*5+x, a*(-1)+y, -3.14 / 12)
+    needle(a*37+x, a*0+y, -3.14 / 7)
 
-needle(320, 512, 3.14 / 3)
-needle(320, 508, 3.14 / 50)
-needle(330, 505, 0 / 18)
-needle(355, 500, 0 / 18)
-needle(367, 500, -3.14 / 25)
-needle(380, 504, -3.14 / 12)
-needle(412, 505, -3.14 / 7)
+    #   яблоки
 
-#   яблоки
+    c.create_oval(a*5+x, a*(-55)+y, a*45+x, a*(-15)+y, outline='white', fill='red', width=0)
+    c.create_oval(a*(-55)+x, a*(-55)+y, a*(-25)+x, a*(-25)+y, outline='white', fill='#fe8800', width=0)
 
-c.create_oval(380, 450, 420, 490, outline='white', fill='red', width=0)
-c.create_oval(320, 450, 350, 480, outline='white', fill='#fe8800', width=0)
+    #   иголки перед туловищем
 
-#   иголки перед туловищем
+    needle(a*(-55)+x, a*20+y, 3.14 / 6)
+    needle(a*(-45)+x, a*22+y, 3.14 / 12)
+    needle(a*(-35)+x, a*15+y, 0 / 18)
+    needle(a*(-20)+x, a*8+y, 0 / 18)
+    needle(a*(-3)+x, a*10+y, -3.14 / 25)
+    needle(a*15+x, a*13+y, -3.14 / 12)
+    needle(a*33+x, a*15+y, -3.14 / 4)
 
-needle(320, 525, 3.14 / 6)
-needle(330, 527, 3.14 / 12)
-needle(340, 520, 0 / 18)
-needle(355, 513, 0 / 18)
-needle(372, 515, -3.14 / 25)
-needle(390, 518, -3.14 / 12)
-needle(408, 520, -3.14 / 4)
-
+hedgehog(20, 200, 400)
 
 
 run()
